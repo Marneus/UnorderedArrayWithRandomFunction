cd47c9d02c805e690e02a9570e347d2b50ca77db
Sebastian Socol <sebastian.socol@gmail.com>
2019-01-25 10:09:23 +0000

    Uploading solution to GitHub

---
+++ RandomFunction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RandomFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] UnorderedArray = CreateUnorderedArray();
+        }
+
+        public static int[] CreateUnorderedArray()
+        {
+            int[] randomArray = new int[1000];
+            int currNumber;
+            int i = 0;
+
+            Random rnd = new Random();
+
+            while (i < 1000)
+            {
+                currNumber = rnd.Next(1, 1001);
+                if (Array.IndexOf(randomArray, currNumber) == -1)
+                {
+                    randomArray[i] = currNumber;
+                    i++;
+                }
+            }
+
+            return randomArray;
+        }
+    }
+}
